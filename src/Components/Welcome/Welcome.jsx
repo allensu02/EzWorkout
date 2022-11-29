0141c40e83e6b4df6ec1cc4b3ab2b66d0cda1f36
Allen Su <allenysu@gmail.com>
2022-11-28 22:42:15 -0500

    fixed

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -9,18 +9,8 @@ export function Welcome() {
         <h1 className='title_style'>
           Welcome to the EzWorkout
         </h1>
-        <div className='subtitle_style'>
-          <p>
-          Please login below!
-          </p> 
-        </div>
       </div>
-
-      <div className='button_div'>
-          <a href="/#/account" className='button_style'>
-            Start App
-          </a>
-      </div>
+      
 
     </div>
   )
